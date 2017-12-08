217033e47c3131542016417b3d7b574346c153f0
Logan Wiser <wiserl@github.com>
2017-12-08 16:52:10 -0700

    first commit, baby

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    
+  
+  <meta charset="UTF-8">
+   
+  
+  <title></title>
+  
+
+
+</head>
+  
+
+
+<body>
+  
+
+<ul>
+    @foreach ($tasks as $task)
+    <li>{{ $task }}</li>
+     @endforeach 
+</ul>
+  
+</body>
+
+
+
+</html>
+
+
