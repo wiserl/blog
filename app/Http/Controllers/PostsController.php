8e01c6168e5b1062c12184e6f3b93073e1b1bee8
Logan Wiser <wiserl@github.com>
2018-01-19 09:13:43 -0700

    upgrading to sierra

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,8 +17,13 @@ class PostsController extends Controller
 
     public function index(){
         $posts = Post::latest()->get();
+        $archives = Post::selectRaw('year(created_at)year, monthname(created_at)month,count(*) published')
+        ->groupBy('year','month')
+        ->orderByRaw('min(created_at) desc')
+        ->get()
+        ->toArray();
 
-        return view('posts.index',compact ('posts'));
+        return view('posts.index',compact ('posts', 'archives'));
     }
 
     public function show(Post $post){
